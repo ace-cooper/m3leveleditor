47a67f532ff8eaf0e0e6b8ec9dd7be4230140e19
Hugo Fagundes <hugo.c.fagundes@gmail.com>
2023-11-20 16:19:56 -0300

    feat: multiple add

--- Assets/Scripts/LevelEditorWindow.cs
+++ Assets/Scripts/LevelEditorWindow.cs
@@ -7,12 +7,18 @@ public class LevelEditorWindow : EditorWindow
 {
     private LevelData currentLevel;
     private List<Tile> tileSlots = new List<Tile>(); // List to hold the tile slots
-    private Tile selectedTile; // Currently selected tile for placement
+
+    private Tile selectedTile; // Tile that is currently selected for placement
+    private int selectedTileIndex = -1; // Index of the selected tile slot
     private Vector2 scrollPosition; // Used for scrolling the level grid
     private Vector2 tilesScrollPosition; // Used for scrolling the tile slots
     private int width = 10; // Default width
     private int height = 10; // Default height
-   private Dictionary<Sprite, Texture2D> spriteTextureCache = new Dictionary<Sprite, Texture2D>(); // Cache to hold sprite textures
+
+    private bool isLeftMouseButtonDown = false;
+    private bool isRightMouseButtonDown = false;
+
+    private Dictionary<Sprite, Texture2D> spriteTextureCache = new Dictionary<Sprite, Texture2D>();
 
     [MenuItem("Match3/Level Editor")]
     public static void ShowWindow()
@@ -83,13 +89,21 @@ public class LevelEditorWindow : EditorWindow
                 EditorGUI.DrawRect(slotRect, Color.grey);
             }
 
+            if (selectedTileIndex == i)
+            {
+                Handles.DrawSolidRectangleWithOutline(slotRect, new Color(0, 0, 0, 0), Color.yellow);
+            }
+
             HandleSlotDragAndDrop(slotRect, i);
 
             // Select the tile for placing when the slot is clicked
             if (GUI.Button(slotRect, GUIContent.none, GUIStyle.none))
             {
                 selectedTile = tileSlots[i];
+                selectedTileIndex = i;
             }
+
+
         }
         EditorGUILayout.EndHorizontal();
         EditorGUILayout.EndScrollView();
@@ -123,9 +137,10 @@ public class LevelEditorWindow : EditorWindow
         }
     }
 
- private void DrawLevelGrid()
+    private void DrawLevelGrid()
 {
     EditorGUILayout.LabelField("Level Grid", EditorStyles.boldLabel);
+    EditorGUI.BeginChangeCheck();
     for (int y = 0; y < currentLevel.height; y++)
     {
         EditorGUILayout.BeginHorizontal();
@@ -133,62 +148,93 @@ public class LevelEditorWindow : EditorWindow
         {
             Rect cellRect = EditorGUILayout.GetControlRect(GUILayout.Width(50), GUILayout.Height(50));
             Tile tile = currentLevel.tiles[x, y];
+
+            // Only try to draw the texture if the sprite is cached
             if (tile != null && tile.sprite != null)
             {
-                Texture2D tex = spriteTextureCache.TryGetValue(tile.sprite, out var cachedTexture) ? cachedTexture : SpriteToTexture(tile.sprite);
-                if (tex != null)
-                {
-                    GUI.DrawTexture(cellRect, tex);
-                }
+                        Texture2D tex;
+                    if (!spriteTextureCache.TryGetValue(tile.sprite, out tex))
+                    {
+                        tex = SpriteToTexture(tile.sprite);
+                        if (tex != null)
+                        {
+                            spriteTextureCache[tile.sprite] = tex;
+                        }
+                    }
+                    
+                    if (tex != null)
+                    {
+                        GUI.DrawTexture(cellRect, tex);
+                    }
             }
-            else if (GUI.Button(cellRect, GUIContent.none, GUIStyle.none))
-            {
-                if (selectedTile != null)
-                {
-                    currentLevel.tiles[x, y] = selectedTile;
-                    // You might need to call Repaint to refresh the editor window
-                    Repaint();
-                }
-            }
-            if (Event.current.type == EventType.Repaint)
+            else if (Event.current.type == EventType.Repaint)
             {
                 EditorGUI.DrawRect(cellRect, tile == null ? Color.gray : Color.white);
+            }
+
+            if ((Event.current.type == EventType.MouseDown || Event.current.type == EventType.MouseDrag) &&
+                cellRect.Contains(Event.current.mousePosition))
+            {
+                if (Event.current.button == 0) // Left click
+                {
+                    if (selectedTile != null && !isRightMouseButtonDown)
+                    {
+                        currentLevel.tiles[x, y] = selectedTile;
+                        isLeftMouseButtonDown = true;
+                    }
+                    Event.current.Use();
+                }
+                else if (Event.current.button == 1) // Right click
+                {
+                    if (!isLeftMouseButtonDown)
+                    {
+                        currentLevel.tiles[x, y] = null;
+                        isRightMouseButtonDown = true;
+                    }
+                    Event.current.Use();
+                }
+            }
+            if (Event.current.type == EventType.MouseUp)
+            {
+                isLeftMouseButtonDown = false;
+                isRightMouseButtonDown = false;
             }
         }
         EditorGUILayout.EndHorizontal();
     }
-}
-
-
-private Texture2D SpriteToTexture(Sprite sprite)
-{
-    if (spriteTextureCache.TryGetValue(sprite, out var cachedTexture))
+    if (EditorGUI.EndChangeCheck())
     {
-        return cachedTexture;
-    }
-
-    if (sprite.texture.isReadable)
-    {
-        // Create a new Texture2D with the correct dimensions
-        Texture2D newText = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
-        // Get the pixels from the sprite and apply them to the new Texture2D
-        Color[] newColors = sprite.texture.GetPixels(
-            (int)sprite.textureRect.x,
-            (int)sprite.textureRect.y,
-            (int)sprite.textureRect.width,
-            (int)sprite.textureRect.height
-        );
-        newText.SetPixels(newColors);
-        newText.Apply();
-        
-        spriteTextureCache[sprite] = newText;
-        return newText;
-    }
-    else
-    {
-        Debug.LogError("Sprite texture is not readable: " + sprite.name);
-        return null;
+        Repaint();
     }
 }
+
+
+ private Texture2D SpriteToTexture(Sprite sprite)
+    {
+        if (spriteTextureCache.TryGetValue(sprite, out var cachedTexture))
+        {
+            return cachedTexture;
+        }
+
+        try
+        {
+            Texture2D newText = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+            Color[] newColors = sprite.texture.GetPixels(
+                (int)sprite.textureRect.x,
+                (int)sprite.textureRect.y,
+                (int)sprite.textureRect.width,
+                (int)sprite.textureRect.height
+            );
+            newText.SetPixels(newColors);
+            newText.Apply();
+            spriteTextureCache[sprite] = newText;
+            return newText;
+        }
+        catch (UnityException e)
+        {
+            Debug.LogError($"Error converting sprite to texture: {e.Message}");
+            return null;
+        }
+    }
 
 }
